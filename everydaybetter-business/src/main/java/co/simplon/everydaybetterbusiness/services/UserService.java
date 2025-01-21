fb4badd4a46f4c2064cda3afb91d0c88ef503e5d
Thao NGUYEN <phuongthaolyon@gmail.com>
2025-01-21 23:58:19 +0100

    add token in cookie when user authenticate

--- everydaybetter-business/src/main/java/co/simplon/everydaybetterbusiness/services/UserService.java
+++ everydaybetter-business/src/main/java/co/simplon/everydaybetterbusiness/services/UserService.java
@@ -3,9 +3,10 @@ package co.simplon.everydaybetterbusiness.services;
 import co.simplon.everydaybetterbusiness.dtos.input.UserAuthenticate;
 import co.simplon.everydaybetterbusiness.dtos.input.UserCreate;
 import co.simplon.everydaybetterbusiness.dtos.output.AuthInfo;
+import jakarta.servlet.http.HttpServletResponse;
 
 public interface UserService {
     void create(UserCreate inputs);
 
-    AuthInfo authenticate(UserAuthenticate inputs);
+    AuthInfo authenticate(UserAuthenticate inputs, HttpServletResponse response);
 }
