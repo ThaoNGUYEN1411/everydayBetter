13f306bcbf3d3c8c843a149389d8472cfe11068c
Thao NGUYEN <phuongthaolyon@gmail.com>
2025-07-25 16:25:40 +0200

    remove unnecessary script

--- everydaybetter-business/src/main/java/co/simplon/everydaybetterbusiness/common/utils/ConvertirUtils.java
+++ everydaybetter-business/src/main/java/co/simplon/everydaybetterbusiness/common/utils/ConvertirUtils.java
@@ -4,10 +4,9 @@ import com.fasterxml.jackson.databind.ObjectMapper;
 import com.fasterxml.jackson.datatype.jsr310.JavaTimeModule;
 
 public final class ConvertirUtils {
-    public static ObjectMapper getMapper(){
+    public static ObjectMapper getMapper() {
         final var mapper = new ObjectMapper();
         mapper.registerModule(new JavaTimeModule());
         return mapper;
     }
-
 }
